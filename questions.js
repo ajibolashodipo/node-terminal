0ff4d4a5cb50b7d4a3a7646b2d8b320b027f72e7
ajibolashodipo <shodipoajibola@gmail.com>
2020-06-08 22:21:13 +0100

    added questions.js

---
+++ questions.js
@@ -0,0 +1,23 @@
+var questions = [
+  "What is your name?",
+  "What would you rather be doing?",
+  "What is your preferred programming language?"
+]
+var answers = []
+function ask(i) {
+  process.stdout.write(`\n\n${questions[i]} >`)
+}
+process.stdin.on("data", function (data) {
+  answers.push(data.toString().trim())
+  if (answers.length < questions.length) {
+    ask(answers.length)
+  } else {
+    process.exit()
+  }
+})
+process.on("exit", function () {
+  process.stdout.write(
+    `\n\n\n${answers[1]} ${answers[0]}, ${answers[2]} is awesome!\n\n`
+  )
+})
+ask(0)

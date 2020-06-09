986e721da2ec9c31d3ddfa4237078abfe34f8458
ajibolashodipo <shodipoajibola@gmail.com>
2020-06-09 20:48:33 +0100

    minor revision

--- mine.js
+++ mine.js
@@ -2,7 +2,7 @@ var events = require("events")
 var eventEmitter = new events.EventEmitter()
 
 eventEmitter.on("helloEvent", () => {
-  console.log("my very first terminal code")
+  console.log("my first terminal code")
 })
 
 eventEmitter.emit("helloEvent")
